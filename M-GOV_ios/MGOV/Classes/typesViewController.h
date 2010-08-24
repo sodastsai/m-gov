9feeb5201b13bfee57634334aea0462607ab3cf6
sodas2002@gmail.com <sodas2002@gmail.com@06d9a83d-5a14-41ab-bbf9-efdc92b8c476>
2010-08-24 12:52:39 +0000

    Merge the type/detail selector into Main project

---
+++ M-GOV_ios/MGOV/Classes/typesViewController.h
@@ -0,0 +1,17 @@
+//
+//  typesViewController.h
+//  M-GOV
+//
+//  Created by sodas on 2010/8/11.
+//  Copyright 2010 NTU Mobile HCI Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface typesViewController : UITableViewController {
+	// Record Question Type
+	NSInteger finalSectionId;
+	NSInteger finalTypeId;
+}
+
+@end
