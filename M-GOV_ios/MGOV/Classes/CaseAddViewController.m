6944b4f76043dae904ed2df54c6e8cb526e3bba3
shou7783@gmail.com <shou7783@gmail.com@06d9a83d-5a14-41ab-bbf9-efdc92b8c476>
2010-08-30 06:33:00 +0000

    Fix UI bug

--- M-GOV_ios/MGOV/Classes/CaseAddViewController.m
+++ M-GOV_ios/MGOV/Classes/CaseAddViewController.m
@@ -64,6 +64,7 @@
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo {
 	[photoButton setBackgroundImage:image forState:UIControlStateNormal];
+	[photoButton setTitle:@"" forState:UIControlStateNormal];
 	[picker dismissModalViewControllerAnimated:YES];
 	self.didSelectPhoto = YES;
 	[self.tableView reloadData];
@@ -127,104 +128,100 @@
 	} else if (indexPath.section == 3 ){
 		return 40;
 	} else if (indexPath.section == 4 ){
-		return 190;
+		return 30;
 	}
 	
-	return 40;
+	return 0;
 }
 
 
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
 	
-	UITableViewCell *cell = nil;
+
 	
-	static NSString *Cell1Identifier = @"Cell";
-	cell = [tableView dequeueReusableCellWithIdentifier:Cell1Identifier];
+	static NSString *CellIdentifier = @"Cell";
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
 	if (cell == nil) {
-		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:Cell1Identifier] autorelease];
+		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
 		// Default Cell Style
 		cell.selectionStyle = UITableViewCellSelectionStyleNone;
-	}
-	
-	// Style by each cell
-	if (indexPath.section == 0) {
-		// TODO: photo scale
-		#pragma mark PhotoPicker
-		if (!didSelectPhoto) {
-			photoButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 300, kPhotoViewHeight)];
 		
-			[photoButton setTitle:@"按一下以加入照片..." forState:UIControlStateNormal];
-			photoButton.titleLabel.font = [UIFont boldSystemFontOfSize:18.0];
-			[photoButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+		// Style by each cell
+		if (indexPath.section == 0) {
+			// TODO: photo scale
+			#pragma mark PhotoPicker
+			if (!didSelectPhoto) {
+				photoButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 300, kPhotoViewHeight)];
+				
+				[photoButton setTitle:@"按一下以加入照片..." forState:UIControlStateNormal];
+				photoButton.titleLabel.font = [UIFont boldSystemFontOfSize:18.0];
+				[photoButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+				
+				photoButton.showsTouchWhenHighlighted = YES;
+				[photoButton addTarget:self action:@selector(photoDialogAction) forControlEvents:UIControlEventTouchUpInside];
+				photoButton.layer.cornerRadius = 10.0;
+				photoButton.layer.masksToBounds = YES;
+				[cell.contentView addSubview:photoButton];
+			}
+		} else if (indexPath.section == 1) {
+			#pragma mark Address MapView
+			MKMapView *mapView = [[MKMapView alloc] initWithFrame:CGRectMake(0, 0, kTextFieldWidth+10, kMapViewHeight-1)];
+			mapView.mapType = MKMapTypeStandard;
+			GlobalVariable *shared = [GlobalVariable sharedVariable];
+			[mapView setCenterCoordinate:shared.locationManager.location.coordinate animated:YES];
+			MKCoordinateRegion region;
+			region.center = shared.locationManager.location.coordinate;
+			MKCoordinateSpan span;
+			span.latitudeDelta = 0.004;
+			span.longitudeDelta = 0.004;
+			region.span = span;
+			mapView.layer.cornerRadius = 10.0;
+			mapView.layer.masksToBounds = YES;
+			[mapView setRegion:region];
 			
-			photoButton.showsTouchWhenHighlighted = YES;
-			[photoButton addTarget:self action:@selector(photoDialogAction) forControlEvents:UIControlEventTouchUpInside];
-			[cell.contentView addSubview:photoButton];
-		} else {
-			[photoButton setTitle:@"" forState:UIControlStateNormal];
-			photoButton.layer.cornerRadius = 10.0;
-			photoButton.layer.masksToBounds = YES;
-		}
-	} else if (indexPath.section == 1) {
-		#pragma mark Address MapView
-		MKMapView *mapView = [[MKMapView alloc] initWithFrame:CGRectMake(0, 0, kTextFieldWidth+10, kMapViewHeight-1)];
-		mapView.mapType = MKMapTypeStandard;
-		GlobalVariable *shared = [GlobalVariable sharedVariable];
-		[mapView setCenterCoordinate:shared.locationManager.location.coordinate animated:YES];
-		MKCoordinateRegion region;
-		region.center = shared.locationManager.location.coordinate;
-		MKCoordinateSpan span;
-		span.latitudeDelta = 0.004;
-		span.longitudeDelta = 0.004;
-		region.span = span;
-		mapView.layer.cornerRadius = 10.0;
-		mapView.layer.masksToBounds = YES;
-		[mapView setRegion:region];
-		
-		// TODO: correct the title: 現在位置or照片位置
-		// TODO: correct the subtitle: 地址
-		AppMKAnnotation *casePlace = [[AppMKAnnotation alloc] initWithCoordinate:region.center andTitle:@"Title test" andSubtitle:@"科科"];
-		[mapView addAnnotation:casePlace];
-		[casePlace release];
-
-		// TODO: fix the bound to round.
-		// TODO: fix the map in normal view		
-		[cell.contentView addSubview:mapView];
-		[mapView release];
-	} else if (indexPath.section == 2) {
-		#pragma mark Type Selector
-		// Decide placeholder or selected result to show
-		if ([selectedTypeTitle length])
-			cell.textLabel.text = selectedTypeTitle;
-		else
-			cell.textLabel.text = @"請按此選擇案件種類";
-		// Other style
-		cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-		cell.selectionStyle = UITableViewCellSelectionStyleBlue;
-	} else if (indexPath.section == 3) {
-		#pragma mark Name field
-		UITextField *nameField = [[UITextField alloc] initWithFrame:CGRectMake(8.0, 8.0, kTextFieldWidth, kTextFieldHeight)];
-		nameField.placeholder = @"請輸入您的姓名";
-		nameField.autocorrectionType = UITextAutocorrectionTypeNo;
-		nameField.delegate = self;
-		nameField.keyboardType = UIKeyboardTypeDefault;
-		nameField.returnKeyType = UIReturnKeyDone;
-		nameField.autocapitalizationType = UITextAutocapitalizationTypeWords;
-		
-		[cell.contentView addSubview:nameField];
-		[nameField release];
-	} else if ( indexPath.section == 4 ){
-		#pragma mark Descritption
-		// TODO: change to other UI element
-		UITextView *descriptionField = [[UITextView alloc] initWithFrame:CGRectMake(8.0, 8.0, kTextFieldWidth, 180)];
-		descriptionField.font = [UIFont systemFontOfSize:18.0];
-		//descriptionField.text = @"請描述案件情況";
-		[cell.contentView addSubview:descriptionField];
-		[descriptionField release];
-		NSLog(@"QQ");
-	} 
-
+			// TODO: correct the title: 現在位置or照片位置
+			// TODO: correct the subtitle: 地址
+			AppMKAnnotation *casePlace = [[AppMKAnnotation alloc] initWithCoordinate:region.center andTitle:@"Title test" andSubtitle:@"科科"];
+			[mapView addAnnotation:casePlace];
+			[casePlace release];
+			
+			// TODO: fix the bound to round.
+			// TODO: fix the map in normal view		
+			[cell.contentView addSubview:mapView];
+			[mapView release];
+		} else if (indexPath.section == 2) {
+			#pragma mark Type Selector
+			// Decide placeholder or selected result to show
+			if ([selectedTypeTitle length])
+				cell.textLabel.text = selectedTypeTitle;
+			else
+				cell.textLabel.text = @"請按此選擇案件種類";
+			// Other style
+			cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+			cell.selectionStyle = UITableViewCellSelectionStyleBlue;
+		} else if (indexPath.section == 3) {
+			#pragma mark Name field
+			UITextField *nameField = [[UITextField alloc] initWithFrame:CGRectMake(8.0, 8.0, kTextFieldWidth, kTextFieldHeight)];
+			nameField.placeholder = @"請輸入您的姓名";
+			nameField.autocorrectionType = UITextAutocorrectionTypeNo;
+			nameField.delegate = self;
+			nameField.keyboardType = UIKeyboardTypeDefault;
+			nameField.returnKeyType = UIReturnKeyDone;
+			nameField.autocapitalizationType = UITextAutocapitalizationTypeWords;
+			
+			[cell.contentView addSubview:nameField];
+			[nameField release];
+		} else if ( indexPath.section == 4 ){
+			#pragma mark Descritption
+			// TODO: change to other UI element
+			UITextView *descriptionField = [[UITextView alloc] initWithFrame:CGRectMake(8.0, 8.0, kTextFieldWidth, 180)];
+			descriptionField.font = [UIFont systemFontOfSize:18.0];
+			//descriptionField.text = @"請描述案件情況";
+			[cell.contentView addSubview:descriptionField];
+			[descriptionField release];
+		} 
+	}
 	return cell;
 }
 
