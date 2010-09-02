72e2d84cd4143a81a44af4a62dd9b68c16b2d637
sodas2002@gmail.com <sodas2002@gmail.com@06d9a83d-5a14-41ab-bbf9-efdc92b8c476>
2010-09-02 06:18:21 +0000

    [iPhone] Add the class of Case Viewer

---
+++ M-GOV_ios/MGOV/Classes/CaseViewerViewController.h
@@ -0,0 +1,16 @@
+//
+//  CaseViewerViewController.h
+//  MGOV
+//
+//  Created by sodas on 2010/9/2.
+//  Copyright 2010 NTU Mobile HCI Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface CaseViewerViewController : UITableViewController {
+
+}
+
+@end
