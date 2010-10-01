4feb19b0d1d314ebac9677a289f49068e878f8ae
sodas2002@gmail.com <sodas2002@gmail.com@06d9a83d-5a14-41ab-bbf9-efdc92b8c476>
2010-10-01 03:33:37 +0000

    [iPhone] new meta CaseSelectorViewController class

---
+++ M-GOV_ios/MGOV/Classes/CaseSelectorViewController.h
@@ -0,0 +1,16 @@
+//
+//  CaseSelectorViewController.h
+//  MGOV
+//
+//  Created by sodas on 2010/10/1.
+//  Copyright 2010 NTU Mobile HCI Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HybridViewController.h"
+
+@interface CaseSelectorViewController : HybridViewController {
+
+}
+
+@end
---
+++ M-GOV_ios/MGOV/Classes/CaseSelectorViewController.m
@@ -0,0 +1,21 @@
+//
+//  CaseSelectorViewController.m
+//  MGOV
+//
+//  Created by sodas on 2010/10/1.
+//  Copyright 2010 NTU Mobile HCI Lab. All rights reserved.
+//
+
+#import "CaseSelectorViewController.h"
+
+/*
+ *
+ *  Use this Class to merge the common section between MyCase and Query views.
+ *  HybridViewController sets most part of common UI section, but contains no Data Source section.
+ *  The common Data Source section is set here.
+ *
+ */
+
+@implementation CaseSelectorViewController
+
+@end
