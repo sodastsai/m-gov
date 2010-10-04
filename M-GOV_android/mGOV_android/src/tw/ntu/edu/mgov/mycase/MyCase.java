edff6069f74cef2971e59d082640cb1e0b952c6a
sodas2002@gmail.com <sodas2002@gmail.com@06d9a83d-5a14-41ab-bbf9-efdc92b8c476>
2010-10-04 09:13:07 +0000

    [Android] Reunit Project

---
+++ M-GOV_android/mGOV_android/src/tw/ntu/edu/mgov/mycase/MyCase.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package tw.ntu.edu.mgov.mycase;
+
+import android.app.Activity;
+
+/**
+ * @author sodas
+ * @company NTU CSIE Mobile HCI Lab
+ *
+ * 2010/10/4
+ */
+public class MyCase extends Activity {
+
+}
---
+++ M-GOV_android/mGOV_android/src/tw/ntu/edu/mgov/querycase/QueryCase.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package tw.ntu.edu.mgov.querycase;
+
+import android.app.Activity;
+
+/**
+ * @author sodas
+ * @company NTU CSIE Mobile HCI Lab
+ *
+ * 2010/10/4
+ */
+public class QueryCase extends Activity {
+
+}
