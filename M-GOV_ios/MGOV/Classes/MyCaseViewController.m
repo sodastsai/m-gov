4ff55efcc8156f6b877b56cb9f8e5f8268f16492
shou7783@gmail.com <shou7783@gmail.com@06d9a83d-5a14-41ab-bbf9-efdc92b8c476>
2010-10-05 11:53:05 +0000

    [iPhone] status 亂舞解決

--- M-GOV_ios/MGOV/Classes/MyCaseViewController.m
+++ M-GOV_ios/MGOV/Classes/MyCaseViewController.m
@@ -150,10 +150,10 @@
 			keyArray = [NSArray arrayWithObjects:@"DataSourceGAEQueryByEmail", nil];
 			combinedStatus = nil;
 		} else if (segmentedControl.selectedSegmentIndex==1) {
-			combinedStatus = [NSString stringWithFormat:@"%d", 0];
+			combinedStatus = [NSString stringWithFormat:@"%d", 1];
 			//combinedStatus = [QueryGoogleAppEngine generateORcombinedCondition:[NSArray arrayWithObjects:@"0",@"4",nil]];
 		} else if (segmentedControl.selectedSegmentIndex==2) {
-			combinedStatus = [NSString stringWithFormat:@"%d", 1];
+			combinedStatus = [NSString stringWithFormat:@"%d", 0];
 			//combinedStatus = [QueryGoogleAppEngine generateORcombinedCondition:[NSArray arrayWithObjects:@"1",@"2",@"3",nil]];
 		} else if (segmentedControl.selectedSegmentIndex==3) {
 			combinedStatus = [NSString stringWithFormat:@"%d", 2];
