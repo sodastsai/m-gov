dff0d68dbd7dd32438f8266cf29e76a9f29c097e
vagrants0140@gmail.com <vagrants0140@gmail.com@06d9a83d-5a14-41ab-bbf9-efdc92b8c476>
2010-10-09 09:46:09 +0000

    [Android] create "SelectLocationMap" Activity and register it in AndroidManifest.

---
+++ M-GOV_android/mGOV_android/src/tw/edu/ntu/mgov/addcase/SelectLocationMap.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package tw.edu.ntu.mgov.addcase;
+
+import com.google.android.maps.MapActivity;
+
+/**
+ * @author vagrants
+ * 2010/10/9
+ * @company NTU CSIE Mobile HCI Lab
+ */
+public class SelectLocationMap extends MapActivity {
+
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see com.google.android.maps.MapActivity#isRouteDisplayed()
+	 */
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
