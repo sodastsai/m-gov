292c7299ce8aae5a5768c76a3b965c763d3ca52f
ggm <godgunman@gmail.com>
2011-03-20 21:38:03 +0800

    Add Fetch case

---
+++ M-GOV_gae2/src/queue/FetchCase.py
@@ -0,0 +1,24 @@
+# Django 1.2
+from google.appengine.dist import use_library
+use_library('django', '1.2')
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import urlfetch
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        url = "http://www.czone2.tcg.gov.tw/GMaps/desc.cfm?sn=09912-704897";
+        result = urlfetch.Fetch(url)
+        if result.status_code == 200:
+            self.response.out.write(result.content)
+        else:
+            self.response.out.write(result.content)
+
+application = webapp.WSGIApplication([('/fetch',MainPage)],debug=True)
+
+def main():
+    run_wsgi_app(application)
+    
+if __name__ == "__main__":
+    main()    
