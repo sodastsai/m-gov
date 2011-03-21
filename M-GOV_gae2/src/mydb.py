01d4a2cfcdbe2e7652f08d4dfcb314aa0ffc7c9f
ggm <godgunman@gmail.com>
2011-03-22 02:45:54 +0800

    change receive property

--- M-GOV_gae2/src/mydb.py
+++ M-GOV_gae2/src/mydb.py
@@ -17,7 +17,7 @@ class Case(db.Model):
     name = db.StringProperty()
     email = db.EmailProperty()
     
-    receive = db.StringProperty()
+    receive = db.StringListProperty()
     date = db.StringProperty()
     photo = db.StringListProperty()
     
