bffd9a28e8be43a5728de7b7073955df4f6c9085
kamebkj <kamebkj@gmail.com>
2011-04-06 20:55:44 +0800

    Add Statistics tab.

---
+++ M-GOV_ios/MGOV/Classes/StatViewController.m
@@ -0,0 +1,72 @@
+/*
+ * 
+ * StatViewController.m
+ * 2011/4/6
+ * kamebkj
+ * 
+ * Statistic web view
+ *
+ * Copyright 2010,2011 NTU CSIE Mobile & HCI Lab
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#import "StatViewController.h"
+
+
+@implementation StatViewController
+
+@synthesize statWebView;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    NSURL *urlToLoad = [NSURL URLWithString:@"http://www.google.com"];
+    NSURLRequest *requestObject = [NSURLRequest requestWithURL:urlToLoad];
+    [statWebView loadRequest:requestObject];
+    //[(UIScrollView*)[statWebView.subviews objectAtIndex:0] setScrollEnabled:NO];
+    [super viewDidLoad];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
