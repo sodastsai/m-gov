c4ec0243b4a87206448780a803863d62ccf263b6
kamebkj <kamebkj@gmail.com>
2011-04-28 10:45:31 +0800

    Correct stat.png and some text

---
+++ M-GOV_ios/MGOV/Classes/Constant.h
@@ -0,0 +1,14 @@
+//
+//  Constant.h
+//  MGOV
+//
+//  Created by Kate Hsiao on 4/7/11.
+//  Copyright 2011 NTU Mobile HCI Lab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@protocol Constant <NSObject>
+
+@end
